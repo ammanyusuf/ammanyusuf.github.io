00b809216f51b0986ac5d45a122cb8c76448b386
Amman Yusuf <46378239+ammanyusuf@users.noreply.github.com>
2023-07-04 01:07:54 -0600

    Fix spelling errors

--- src/data/user.js
+++ src/data/user.js
@@ -19,7 +19,7 @@ const INFO = {
 		description:
 			"Hello. I am Amman and welcome to my website. My experience has touched nearly all aspects of computer science. " + 
 			"I built scalable web applications, performed UI/UX work, analyzed, displayed and collected data, trained machine learning models, " +
-			"handled distributed applications, and traversed  backend worked with ease (not so easy while doing it). I am a passionate learner and " + 
+			"handled distributed applications, and traversed  backend work with ease (not so easy while doing it). I am a passionate learner and " + 
 			"always look to tackle challenging problems. My curiosity  moves me from project to project, but I never fail to land on new challenges and " +
 			"exciting problems to overcome as a computer scientist 🧑‍💻"
 	},
@@ -32,14 +32,14 @@ const INFO = {
 			"Through my degree I took on various projects and internships. While varied, the common theme between my experience is challenging, interesting, and fulfilling work. " + 
 			"I started developing websites (I still do) in my first role, which got me involved in AWS, which then got working with data in my data science role, which opened up my " +
 			"interest in machine learning, which got me interested in computer vision and deep learning, which culminated (so far) in my final year thesis project in Federated Learning. " +
-			"Apart from work experience, I still have a great interest in distributed applications and theory. One of my favourite courses from my degree was 'Introduction to Distributed Algorithms " + 
+			"Apart from work experience, I still have a great interest in distributed applications and theory. One of my favourite courses from my degree was 'Introduction to Distributed Algorithms' " + 
 			"where we proved the correctness of distributed algorithms. It was a delight understanding how the algorithms work and the proofs at the end were no short of amazing.",
 	},
 
 	articles: {
 		title: "I partake in blogging 🕵️",
 		description:
-			"Some throughts and ideas I like to type out. They are related to coding, personal projects, or other random stuff I find.",
+			"Some thoughts and ideas I like to type out. They are related to coding, personal projects, or other random stuff I find.",
 	},
 
 	projects: [
